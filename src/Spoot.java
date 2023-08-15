caf8348005d3229dbf33196c2fca2b28c72ac1f0
theoliverlear <sigwartholiver2@gmail.com>
2023-08-15 13:23:20 -0500

    Add comment header and add Spoot class hierarchy ideas

---
+++ src/Spoot.java
@@ -0,0 +1,39 @@
+// Oliver Sigwarth                                                           |
+// Computer Programming (CIDS 162)                                           |
+// Assignment 3 - The Magical Object Oriented Spoot                          |
+// Program: Spoot.java                                                       |
+// Description:                                                              |
+/*---------------------------------------------------------------------------|
+| In this assignment, you will create a Java program to explore the concepts |
+| of basic classes, constructors, getters, and setters. Instead of using     |
+| real-world entities, you will design a class hierarchy representing        |
+| imaginary creatures called Spoots. These Spoots will have specific         |
+| attributes such as name, color and age. Your objective is to demonstrate   |
+| your understanding of class design and object-oriented programming         |
+| concepts by implementing the Spoot class.                                  |
+----------------------------------------------------------------------------*/
+// Created: 8/15/2023                                                        |
+// Last Modified: 8/15/2023                                                  |
+//----------------------------------------------------------------------------
+
+// TODO: Imagine different Spoots. They will relate to
+//       each other through their shared attribues of
+//       their superclasses. They will also be unique
+//       in their own ways and how they may interpret
+//       their superclasses' attributes.
+
+// Idea 1: Essence Spoot (Abstract class???)
+//         Essence Spoots are the most basic Spoots.
+//         They are like ghosts or phantoms. These
+//         Spoots may rank higher on the hierarchy
+//         than other Spoots because they are mostly
+//         the soul of a Spoot and they have
+//         transcended the physical realm.
+
+
+public class Spoot {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
