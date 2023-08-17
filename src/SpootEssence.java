bac1921eb39c9e094b2efd68a5909860fbaad211
theoliverlear <sigwartholiver2@gmail.com>
2023-08-17 17:03:28 -0500

    Fill in class with methods, constructors, and variables

--- src/SpootEssence.java
+++ src/SpootEssence.java
@@ -1,2 +1,57 @@
-public class SpootEssence {
+// Oliver Sigwarth                                                           |
+// Computer Programming (CIDS 162)                                           |
+// Assignment 3 - The Magical Object Oriented Spoot                          |
+// Program: Spoot.java                                                       |
+// Description:                                                              |
+/*---------------------------------------------------------------------------|
+| In this assignment, you will create a Java program to explore the concepts |
+| of basic classes, constructors, getters, and setters. Instead of using     |
+| real-world entities, you will design a class hierarchy representing        |
+| imaginary creatures called Spoots. These Spoots will have specific         |
+| attributes such as name, color and age. Your objective is to demonstrate   |
+| your understanding of class design and object-oriented programming         |
+| concepts by implementing the Spoot class.                                  |
+----------------------------------------------------------------------------*/
+// Created: 8/15/2023                                                        |
+// Last Modified: 8/17/2023                                                  |
+//---------------------------------------------------------------------------|
+public abstract class SpootEssence {
+    //-----------------------------Class-Variables----------------------------
+    private String name;
+    private String color; // TODO: Console corresponds to color
+    private int age;
+    //--------------------------Argument-Constructor--------------------------
+    public SpootEssence(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+    //--------------------------Default-Constructor--------------------------
+    SpootEssence() {
+        this.name = "Spoot";
+        this.color = "white";
+        this.age = 1;
+    }
+    //-------------------------------Methods----------------------------------
+    public abstract void details();
+    //-------------------------------Getters----------------------------------
+    public String getName() {
+        return this.name;
+    }
+    public String getColor() {
+        return this.color;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    //-------------------------------Setters----------------------------------
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
