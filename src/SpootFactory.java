7aed3d731ec88868b36994925d3c7d9d09402027
theoliverlear <sigwartholiver2@gmail.com>
2023-08-17 23:52:22 -0500

    Add comments and test objects

--- src/SpootFactory.java
+++ src/SpootFactory.java
@@ -1,7 +1,7 @@
 // Oliver Sigwarth                                                           |
 // Computer Programming (CIDS 162)                                           |
 // Assignment 3 - The Magical Object Oriented Spoot                          |
-// Program: Spoot.java                                                       |
+// Program: SpootFactory.java                                                |
 // Description:                                                              |
 /*---------------------------------------------------------------------------|
 | In this assignment, you will create a Java program to explore the concepts |
@@ -13,16 +13,38 @@
 | concepts by implementing the Spoot class.                                  |
 ----------------------------------------------------------------------------*/
 // Created: 8/15/2023                                                        |
-// Last Modified: 8/16/2023                                                  |
+// Last Modified: 8/17/2023                                                  |
 //---------------------------------------------------------------------------|
+/*
+Instructions:
 
-// TODO: Imagine different Spoots. They will relate to
-//       each other through their shared attribues of
-//       their superclasses. They will also be unique
-//       in their own ways and how they may interpret
-//       their superclasses' attributes.
+1.Design the Spoot class with Constructors, Getters, and Setters:
 
-// Idea 1: Essence Spoot (Abstract class???)
+   - Create a class called Spoot with the following attributes: name, color,
+     weight and age.
+   - Define constructors to initialize the Spoot objects with relevant
+     attributes.
+   - Implement appropriate getter and setter methods to access and modify
+     Spoot attributes.
+   - Create a method called "details" that will return the name, color, weight
+     and age like so: "My name is Spooty. I am 4 years old. I am a blue Spoot
+     and I weigh 2 lbs."
+3. Test Program:
+   - Create a main class called Main to simulate interactions with the Spoot
+     objects.
+   - Instantiate 3 different Spoot and use the description method on each of
+     them.
+
+4. Documentation:
+   - Include comments in your code to explain the purpose and functionality
+     of classes, methods, and significant blocks of code.
+   - Write a header comment at the beginning of each file, including your name
+    and a brief description of the assignment.
+ */
+
+
+
+// Idea 1: Essence Spoot
 //         Essence Spoots are the most basic Spoots.
 //         They are like ghosts or phantoms. These
 //         Spoots may rank higher on the hierarchy
@@ -31,9 +53,33 @@
 //         transcended the physical realm.
 
 
+import java.util.ArrayList;
+
 public class SpootFactory {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //------------------------Normal-Spoot--------------------------------
+        Spoot normalSpoot = new Spoot("Sarah", "blue", 4, 2);
+        System.out.println(normalSpoot.details());
+        //------------------------Super-Spoot---------------------------------
+        ArrayList<String> powers = new ArrayList<>();
+        powers.add("super cuteness");
+        powers.add("super speed");
+        powers.add("super sparkle");
+        SuperSpoot superSpoot = new SuperSpoot("Oliver", "white", 3, 4,
+                                               powers);
+        System.out.println(superSpoot.details());
+        //------------------------Wizard-Spoot--------------------------------
+        WizardSpoot wizardSpoot = new WizardSpoot("Gandalf", "grey", 1000, 7);
+        System.out.println(wizardSpoot.details());
+        wizardSpoot.ageSpell(6, normalSpoot);
+        wizardSpoot.nameSpell();
+        //----------------------Government-Spoot------------------------------
+        GovernmentSpoot governmentSpoot = new GovernmentSpoot("Agent Spooter",
+                                                              "black", 5, 3,
+                                           "Department of Spoots");
+        System.out.println(governmentSpoot.details());
+        governmentSpoot.fileNewName("Susan", normalSpoot);
+
     }
 }
 
