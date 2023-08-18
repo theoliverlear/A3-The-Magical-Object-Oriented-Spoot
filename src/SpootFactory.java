5c368a146c51c73a35e51758a3abeb5fe9039f50
theoliverlear <sigwartholiver2@gmail.com>
2023-08-18 02:03:45 -0500

    Add test objects in a test method and add final comments

--- src/SpootFactory.java
+++ src/SpootFactory.java
@@ -13,50 +13,34 @@
 | concepts by implementing the Spoot class.                                  |
 ----------------------------------------------------------------------------*/
 // Created: 8/15/2023                                                        |
-// Last Modified: 8/17/2023                                                  |
+// Last Modified: 8/18/2023                                                  |
 //---------------------------------------------------------------------------|
-/*
-Instructions:
 
-1.Design the Spoot class with Constructors, Getters, and Setters:
-
-   - Create a class called Spoot with the following attributes: name, color,
-     weight and age.
-   - Define constructors to initialize the Spoot objects with relevant
-     attributes.
-   - Implement appropriate getter and setter methods to access and modify
-     Spoot attributes.
-   - Create a method called "details" that will return the name, color, weight
-     and age like so: "My name is Spooty. I am 4 years old. I am a blue Spoot
-     and I weigh 2 lbs."
-3. Test Program:
-   - Create a main class called Main to simulate interactions with the Spoot
-     objects.
-   - Instantiate 3 different Spoot and use the description method on each of
-     them.
-
-4. Documentation:
-   - Include comments in your code to explain the purpose and functionality
-     of classes, methods, and significant blocks of code.
-   - Write a header comment at the beginning of each file, including your name
-    and a brief description of the assignment.
- */
-
-
-
-// Idea 1: Essence Spoot
-//         Essence Spoots are the most basic Spoots.
-//         They are like ghosts or phantoms. These
-//         Spoots may rank higher on the hierarchy
-//         than other Spoots because they are mostly
-//         the soul of a Spoot and they have
-//         transcended the physical realm.
-
-
+/*----------------------------------------------------------------------------
+| 4b. Write a header comment at the beginning of each file, including your   |
+|     name and a brief description of the assignment.                        |
+----------------------------------------------------------------------------*/
 import java.util.ArrayList;
-
+/*----------------------------------------------------------------------------
+| 3a. Create a main class called Main to simulate interactions with the      |
+|     Spoot objects.                                                         |
+ ---------------------------------------------------------------------------*/
 public class SpootFactory {
-    public static void main(String[] args) {
+    // The SpootFactory class is where you can see the hub of Spoot creation.
+    // Several kinds of Spoots are created and their methods are demonstrated
+    // in this class.
+    /*------------------------------------------------------------------------
+    | 4a. Include comments in your code to explain the purpose and           |
+    |     functionality of classes, methods, and significant blocks of code. |
+    |------------------------------------------------------------------------|
+    | 3. Test Program.                                                       |
+    ------------------------------------------------------------------------*/
+    public static void testSpootCreation() {
+        /*--------------------------------------------------------------------
+        | 3b. Instantiate 3 different Spoot and use the description method   |
+        |     on each of them.                                               |
+        --------------------------------------------------------------------*/
+
         //------------------------Normal-Spoot--------------------------------
         Spoot normalSpoot = new Spoot("Sarah", "blue", 4, 2);
         System.out.println(normalSpoot.details());
@@ -79,7 +63,16 @@ public class SpootFactory {
                                            "Department of Spoots");
         System.out.println(governmentSpoot.details());
         governmentSpoot.fileNewName("Susan", normalSpoot);
-
+    }
+    //-----------------------------Main-Method--------------------------------
+    public static void main(String[] args) {
+        // This method will run the entirety of the program while utilizing
+        // other methods as well.
+        testSpootCreation();
+        // Read README.md to run the program.
+        /*--------------------------------------------------------------------
+        | 4. Documentation.                                                  |
+        --------------------------------------------------------------------*/
     }
 }
 
