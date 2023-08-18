2370ec86ed70cbea4364ef1d596f2c139a939a33
theoliverlear <sigwartholiver2@gmail.com>
2023-08-18 06:55:46 -0500

    Add handling for file structure navigation

--- src/run_spoot_factory.sh
+++ src/run_spoot_factory.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #enter chmod +x run_spoot_factory.sh
 
-javac SpootFactory.java
+javac -d ../out SpootFactory.java
 if [ $? -eq 0 ]
 then
   echo "Welcome to the Spoot Factory!"
-  java SpootFactory
+  java -cp ../out SpootFactory
 else
   echo "Failed to load program"
 fi
